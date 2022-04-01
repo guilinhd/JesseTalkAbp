5586da8aa6e15036da10fd66d2b706123abc16d9
guilinhd <394811364@qq.com>
2022-04-01 16:17:37 +0800

    完成webapidemo，引用volo.apb.core volo.autofac，需要在programm 使用autofac替换默认的依赖注入框架

---
+++ src/BasicDemo/WebApiDemo/WebApiModule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp;
+using Volo.Abp.Modularity;
+using Volo.Abp.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiDemo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp.Autofac;
+
+namespace WebApiDemo
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreModule),
+        typeof(AbpAutofacModule)
+    )]
+    public class WebApiModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var services = context.Services;
+
+            services.AddTransient<HelloAbpService>();
+            services.AddControllers();
+
+            //base.ConfigureServices(context);
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
