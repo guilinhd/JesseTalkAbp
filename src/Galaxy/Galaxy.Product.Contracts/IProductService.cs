5a835075e6de4171d3129448e46e8053cef90593
guilinhd <394811364@qq.com>
2022-04-02 10:07:35 +0800

    完成Product.Contracts的项目

---
+++ src/Galaxy/Galaxy.Product.Contracts/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Services;
+using System.Threading;
+using System.Threading.Tasks;
+using Galaxy.Product.Contracts.Models.Dtos;
+
+namespace Galaxy.Product.Contracts
+{
+    public interface IProductService : IApplicationService
+    {
+        Task<ProductDto> CreateAsync(ProductDto productDto);
+
+        Task<IEnumerable<ProductDto>> GetAllAsync(ProductQueryDto query);
+    }
+}
---
+++ src/Galaxy/Galaxy.Product.Contracts/Models/Dtos/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy.Product.Contracts.Models.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
