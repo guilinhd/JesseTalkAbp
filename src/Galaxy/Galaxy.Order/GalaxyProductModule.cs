83fd4b104f4ee083f5fc24a968b16ad77da831be
guilinhd <394811364@qq.com>
2022-04-02 10:35:42 +0800

    完成OrderService模块

---
+++ src/Galaxy/Galaxy.Order/GalaxyProductModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Modularity;
+
+namespace Galaxy.Order
+{
+    public class GalaxyProductModule : AbpModule
+    {
+    }
+}
--- src/Galaxy/Galaxy.Order/OrderService.cs
+++ src/Galaxy/Galaxy.Order/OrderService.cs
@@ -4,10 +4,20 @@ using System.Text;
 using Volo.Abp.Application.Services;
 using Galaxy.Order.Contracts.Models.Dtos;
 using Galaxy.Order.Contracts;
+using System.Threading.Tasks;
 
 namespace Galaxy.Order
 {
     public class OrderService : ApplicationService, IOrderService
     {
+        public Task<OrderDto> CreateAsync(OrderDto orderDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDto> FindAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
