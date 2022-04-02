0006eb642b788efe596942abc7d4cd9f48683ee3
guilinhd <394811364@qq.com>
2022-04-02 10:24:45 +0800

    完成order.contracts模块

---
+++ src/Galaxy/Galaxy.Order.Contracts/IOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Services;
+using System.Threading;
+using System.Threading.Tasks;
+using Galaxy.Order.Contracts.Models.Dtos;
+
+namespace Galaxy.Order.Contracts
+{
+    public interface IOrder : IApplicationService
+    {
+        Task<OrderDto> CreateAsync(OrderDto orderDto);
+
+        Task<OrderDto> FindAsync(int id);
+    }
+}
---
+++ src/Galaxy/Galaxy.Order.Contracts/Models/Dtos/OrderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Galaxy.Product.Contracts.Models.Dtos;
+
+namespace Galaxy.Order.Contracts.Models.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { set; get; }
+
+        public string Name { set; get; }
+
+        public IEnumerable<ProductDto> Products { set; get; }
+    }
+}
