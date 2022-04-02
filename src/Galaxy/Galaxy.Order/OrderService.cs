d06a18fff8575e5c8276d9c1c7acfe37aa9ed0af
guilinhd <394811364@qq.com>
2022-04-02 10:30:32 +0800

    IOrder规范命名成IOrderService

---
+++ src/Galaxy/Galaxy.Order/OrderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Services;
+using Galaxy.Order.Contracts.Models.Dtos;
+using Galaxy.Order.Contracts;
+
+namespace Galaxy.Order
+{
+    public class OrderService : ApplicationService, IOrderService
+    {
+    }
+}
