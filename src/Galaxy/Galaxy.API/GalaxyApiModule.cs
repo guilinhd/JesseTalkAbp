f2b8786561e59fcadc8ead289fbb9f33cdb7fafc
guilinhd <394811364@qq.com>
2022-04-02 10:46:12 +0800

    更改GalaxyOrderModule命名

---
+++ src/Galaxy/Galaxy.API/GalaxyApiModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Modularity;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.Autofac;
+using Galaxy.Order;
+using Galaxy.Product;
+
+namespace Galaxy.API
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreModule),
+        typeof(AbpAutofacModule)
+    )]
+    [DependsOn(
+        typeof(GalaxProductModule),
+        typeof(GalaxOrderModule)
+    )]
+
+    public class GalaxyApiModule : AbpModule
+    {
+    }
+}
