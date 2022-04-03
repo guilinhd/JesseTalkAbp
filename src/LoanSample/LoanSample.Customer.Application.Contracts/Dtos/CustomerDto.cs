76b514becdc60bba45245cce0297d105620d6bd5
guilinhd <394811364@qq.com>
2022-04-03 16:57:15 +0800

    完成customer.application.contracts的模块

---
+++ src/LoanSample/LoanSample.Customer.Application.Contracts/Dtos/CustomerDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanSample.Customer.Application.Contracts.Dtos
+{
+    public class CustomerDto
+    {
+        public Guid Id { set; get; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string IdNo { get; set; }
+
+        public List<LinkmanDto> Linkman { get; set; }
+    }
+
+    public class LinkmanDto
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string IdNo { get; set; }
+
+    }
+}
---
+++ src/LoanSample/LoanSample.Customer.Application.Contracts/ICustomerAppService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using LoanSample.Customer.Application.Contracts.Dtos;
+
+namespace LoanSample.Customer.Application.Contracts
+{
+    public interface ICustomerAppService : IApplicationService
+    {
+        Task<CustomerDto> GetListAsync();
+
+        Task<CustomerDto> CreateAsync(CustomerDto customerDto);
+
+        Task<CustomerDto> AddLinkman(Guid customerId, LinkmanDto linkman);
+    }
+}
