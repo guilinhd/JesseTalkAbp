8cf0726b871facb27dd71d8a3368103aefa799cf
guilinhd <394811364@qq.com>
2022-04-03 15:48:57 +0800

    完成domain模块功能

---
+++ src/LoanSample/LoanSample.Domain/Customers/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Volo.Abp.Domain.Entities;
+
+namespace LoanSample.Domain.Customers
+{
+    public class Customer : AggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string IdNo { get; set; }
+
+        private List<Linkman> _linkman = new List<Linkman>();
+
+        public ReadOnlyCollection<Linkman> Linkman()
+        {
+            return linkman.AsReadOnly();
+        }
+
+        public void AddLinkman(Linkman linkman)
+        {
+            _linkman.Add(linkman);
+        }
+    }
+}
---
+++ src/LoanSample/LoanSample.Domain/Customers/Linkman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace LoanSample.Domain.Customers
+{
+    public class Linkman : Entity<Guid>
+    {
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
