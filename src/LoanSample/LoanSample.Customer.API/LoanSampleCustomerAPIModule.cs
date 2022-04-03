746ec63bc484a8701fd7b1f07b31598c8a116ed6
guilinhd <394811364@qq.com>
2022-04-03 22:46:22 +0800

    完成api模块的搭建

---
+++ src/LoanSample/LoanSample.Customer.API/LoanSampleCustomerAPIModule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Modularity;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.Autofac;
+using LoanSample.Customer.Application;
+using Volo.Abp;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Volo.Abp.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoanSample.Customer.API
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreModule),
+        typeof(AbpAutofacModule)
+        )]
+
+    [DependsOn(typeof(LoanSampleCustomerApplicationModule))]
+
+    public class LoanSampleCustomerAPIModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpAspNetCoreMvcOptions>(options => {
+                options.ConventionalControllers.Create(typeof(LoanSampleCustomerApplicationModule).Assembly);
+            });
+
+            ConfigureSwaggerServices(context.Services);
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "LoanSample.Customer.API");
+                });
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void ConfigureSwaggerServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "LoanSample.Customer.API", Description = "V1.0" });
+                options.DocInclusionPredicate((doc, description) => true);
+                options.CustomSchemaIds(type => type.FullName);
+            });
+        }
+    }
+}
---
+++ src/LoanSample/LoanSample.Customer.API/Startup.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoanSample.Customer.API
+{
+    public class Startup
+    {
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplication<LoanSampleCustomerAPIModule>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.InitializeApplication();
+        }
+    }
+}
