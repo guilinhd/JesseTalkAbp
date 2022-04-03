25746cd9e83ed5350bc3fd350111a0f7aa06d202
guilinhd <394811364@qq.com>
2022-04-03 17:04:58 +0800

    完成customer.application模块

---
+++ src/LoanSample/LoanSample.Customer.Application/CustomerAppService.cs
@@ -0,0 +1,29 @@
+﻿using LoanSample.Customer.Application.Contracts;
+using LoanSample.Customer.Application.Contracts.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace LoanSample.Customer.Application
+{
+    public class CustomerAppService : ApplicationService, ICustomerAppService
+    {
+        public Task<CustomerDto> AddLinkman(Guid customerId, LinkmanDto linkman)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerDto> CreateAsync(CustomerDto customerDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomerDto> GetListAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
