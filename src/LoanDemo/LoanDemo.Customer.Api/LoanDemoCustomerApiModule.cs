d3611d91735f29e856c69935b1add5f895bb26bb
guilinhd <394811364@qq.com>
2022-04-04 10:44:27 +0800

    增加customer.api 模块

---
+++ src/LoanDemo/LoanDemo.Customer.Api/LoanDemoCustomerApiModule.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Volo.Abp.Modularity;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Autofac;
+using Volo.Abp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace LoanDemo.Customer.Api
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreMvcModule),
+        typeof(AbpAutofacModule)
+        )]
+    public class LoanDemoCustomerApiModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var services = context.Services;
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LoanDemo.Customer.Api", Version = "v1" });
+                c.DocInclusionPredicate((doc, description) => true);
+                c.CustomSchemaIds(type => type.FullName);
+            });
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LoanDemo.Customer.Api v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
