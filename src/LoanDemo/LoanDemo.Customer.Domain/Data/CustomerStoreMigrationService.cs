1bebbc4b7ebd042b53787626c26a46f296d0ec22
guilinhd <394811364@qq.com>
2022-04-04 17:16:54 +0800

    完成domain模块的

---
+++ src/LoanDemo/LoanDemo.Customer.Domain/Data/CustomerStoreMigrationService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace LoanDemo.Customer.Domain.Data
+{
+    public class CustomerStoreMigrationService : ITransientDependency
+    {
+        private readonly ICustomerStoreSchemaMigrator _dbSchemaMigrator;
+        public ILogger<CustomerStoreMigrationService> Logger { get; set; }
+
+        public CustomerStoreMigrationService(ICustomerStoreSchemaMigrator dbSchemaMigrator)
+        {
+            _dbSchemaMigrator = dbSchemaMigrator;
+        }
+
+        public async Task MigratorAsync()
+        {
+
+            Logger.LogInformation("Started database migrations...");
+            Logger.LogInformation("Migrating database schema...");
+
+            await _dbSchemaMigrator.MigratorAsync();
+
+            Logger.LogInformation("Successfully completed database migrations.");
+        }
+    }
+}
