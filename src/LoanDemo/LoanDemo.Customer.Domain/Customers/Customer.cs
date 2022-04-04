99846010527941fa3c19729ce109f55fc0655ff2
guilinhd <394811364@qq.com>
2022-04-04 11:22:29 +0800

    添加customer.domain 模块

---
+++ src/LoanDemo/LoanDemo.Customer.Domain/Customers/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace LoanDemo.Customer.Domain.Customers
+{
+    public class Customer : AggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string IdNo { get; set; }
+
+        private List<Linkman> Linkmen = new List<Linkman>();
+        public ReadOnlyCollection<Linkman> linkman()
+        {
+            return Linkmen.AsReadOnly();
+        }
+
+        public void AddLinkman(Linkman linkman)
+        {
+            Linkmen.Add(linkman);
+        }
+    }
+
+    public class Linkman : Entity<Guid>
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string IdNo { get; set; }
+    }
+}
