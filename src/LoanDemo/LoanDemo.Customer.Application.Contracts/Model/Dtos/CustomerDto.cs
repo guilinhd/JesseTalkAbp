f58338229591bb90de61f3439e0bda95454ffe98
guilinhd <394811364@qq.com>
2022-04-04 11:24:37 +0800

    完成customer.application.contracts中dto的属性

--- src/LoanDemo/LoanDemo.Customer.Application.Contracts/Model/Dtos/CustomerDto.cs
+++ src/LoanDemo/LoanDemo.Customer.Application.Contracts/Model/Dtos/CustomerDto.cs
@@ -8,12 +8,22 @@ namespace LoanDemo.Customer.Application.Contracts.Model.Dtos
 {
     public class CustomerDto
     {
+        public string Name { get; set; }
 
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string IdNo { get; set; }
+
+        public List<LinkmanDto> linkmen { get; set; }
 
     }
 
     public class LinkmanDto
     {
-
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string IdNo { get; set; }
     }
 }
