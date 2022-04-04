ca4b335da428ffc678fdd56b034c2f8ff7893190
guilinhd <394811364@qq.com>
2022-04-04 15:31:05 +0800

    完成 customer.application 模块中对automapper的相关设置

--- src/LoanDemo/LoanDemo.Customer.Application/LoanDemoCustomerApplicationModule.cs
+++ src/LoanDemo/LoanDemo.Customer.Application/LoanDemoCustomerApplicationModule.cs
@@ -4,10 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Modularity;
+using Volo.Abp.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using LoanDemo.Customer.Application.Contracts;
+using LoanDemo.Customer.Domain;
 
 namespace LoanDemo.Customer.Application
 {
+    [DependsOn(
+        typeof(AbpAutoMapperModule)
+        )]
+
+    [DependsOn(
+        typeof(LoanDemoCustomerDomainModule),
+        typeof(LoanDemoCustomerApplicationContractsModule)
+        )]
     public class LoanDemoCustomerApplicationModule : AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var services = context.Services;
+
+            services.AddAutoMapperObjectMapper<LoanDemoCustomerApplicationModule>();
+
+            Configure<AbpAutoMapperOptions>(options => {
+                options.AddMaps<LoanDemoCustomerApplicationModule>();
+            });
+        }
     }
 }
